56acf72ae244d60073df05e8e446ec83b0fa1bc6
jerh <fifwish@gmail.com>
2012-05-05 23:03:33 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RetwisRails::Application.config.secret_token = '544548627f817cce79de7c4bf3fb5f90f884369832f9c8a53ec670afa239a1a1ee05034e9a275dbde49469ca577289853debdf51c223f4341fde4d4182c110d4'
